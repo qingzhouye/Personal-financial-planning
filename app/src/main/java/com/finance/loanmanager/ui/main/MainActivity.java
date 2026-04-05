d6ba9ba00a92210bd0c4ad89bd96f3a6f3d65767
creat-dream <2330619978@qq.com>
2026-04-05 21:12:26 +0800

    取消应用名称显示

--- app/src/main/java/com/finance/loanmanager/ui/main/MainActivity.java
+++ app/src/main/java/com/finance/loanmanager/ui/main/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
